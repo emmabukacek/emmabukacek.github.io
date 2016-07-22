9925e0d1ad396583d6f0669e2462d34409c33c35
Emma Bukacek <emma.bukacek@gmail.com>
2016-07-22 10:33:43 -0500

    Oh great, now a build script; damned kids
    And their shitty memes
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
    ░░░░░░░░░░░░░▄▄▄▄▄▄▄░░░░░░░░░
    ░░░░░░░░░▄▀▀▀░░░░░░░▀▄░░░░░░░
    ░░░░░░░▄▀░░░░░░░░░░░░▀▄░░░░░░
    ░░░░░░▄▀░░░░░░░░░░▄▀▀▄▀▄░░░░░
    ░░░░▄▀░░░░░░░░░░▄▀░░██▄▀▄░░░░
    ░░░▄▀░░▄▀▀▀▄░░░░█░░░▀▀░█▀▄░░░
    ░░░█░░█▄▄░░░█░░░▀▄░░░░░▐░█░░░
    ░░▐▌░░█▀▀░░▄▀░░░░░▀▄▄▄▄▀░░█░░
    ░░▐▌░░█░░░▄▀░░░░░░░░░░░░░░█░░
    ░░▐▌░░░▀▀▀░░░░░░░░░░░░░░░░▐▌░
    ░░▐▌░░░░░░░░░░░░░░░▄░░░░░░▐▌░
    ░░▐▌░░░░░░░░░▄░░░░░█░░░░░░▐▌░
    ░░░█░░░░░░░░░▀█▄░░▄█░░░░░░▐▌░
    ░░░▐▌░░░░░░░░░░▀▀▀▀░░░░░░░▐▌░
    ░░░░█░░░░░░░░░░░░░░░░░░░░░█░░
    ░░░░▐▌▀▄░░░░░░░░░░░░░░░░░▐▌░░
    ░░░░░█░░▀░░░░░░░░░░░░░░░░▀░░░
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░

---
+++ scripts/release.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# ---
+# RELEASE SCRIPT
+# ---
+#
+# It's shitty and weird.
+# (•_•)
+# ( •_•)>⌐■-■
+# (⌐■_■)
+
+graceful_exit() {
+  git reset HEAD .
+  git checkout -- .
+  exit 1
+}
+
+npm run build
+git checkout release || exit 1
+
+mkdir __temp
+cp dist/* __temp
+cp -r fonts/ __temp
+git checkout -- .
+
+git checkout master
+cp -rf __temp/* .
+rm -rf __temp
+git add .
+
+git commit || graceful_exit
+
+git push
